4e5f75964830b8be12ac8e10226536d0f0a087d0
Igna2017cio <126526088+Igna2017cio@users.noreply.github.com>
2023-02-28 11:13:45 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+
+function mostrar () {
+    oculto1.style.display = 'block';
+    boton1.style.display = 'none';
+}
+function ocultar () {
+    boton1.style.display = 'block';
+    oculto1.style.display = 'none';
+}
+
+document.getElementById ("boton1").onclick 
+    oculto1.style.display = "block";
